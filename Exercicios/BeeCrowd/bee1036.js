94ae27f8a052cc700ab72852def61b80fd263a42
mgluz <miguelmluz777@gmail.com>
2024-12-04 17:24:56 -0300

    att

---
+++ Exercicios/BeeCrowd/bee1036.js
@@ -0,0 +1,46 @@
+var input = require('fs').readFileSync('./input.txt', 'utf8');
+var lines = input.split('\n');
+
+var abc = lines[0].split(' ');
+var a = abc[0];
+
+if (a === 0) {
+    console.log("Impossivel calcular");
+
+} else {
+
+var b = abc[1];
+var c = abc[2];
+
+var D = b * b - 4 * a * c;
+
+if (D < 0) {
+    console.log("Impossivel calcular");
+
+} else {
+
+var R1 = (-b + Math.sqrt(D)) / (2 * a);
+var R2 = (-b - Math.sqrt(D)) / (2 * a);
+
+console.log(`R1 = ${R1.toFixed(5)}`);
+console.log(`R2 = ${R2.toFixed(5)}`);
+ }
+}
+
+// var abc = lines[0].split(' ');
+// var a = parseFloat(abc[0]);
+// var b = parseFloat(abc[1]);
+// var c = parseFloat(abc[2]);
+// var D = b * b - 4 * a * c;
+
+// if (a === 0 || (b * b - 4 * a * c) < 0) {
+//     console.log("Impossivel calcular");
+// } else {
+
+// var R1 = (-b + Math.sqrt(D)) / (2 * a);
+// var R2 = (-b - Math.sqrt(D)) / (2 * a);
+
+// console.log(`R1 = ${R1.toFixed(5)}`);
+// console.log(`R2 = ${R2.toFixed(5)}`);
+//  }
+// }

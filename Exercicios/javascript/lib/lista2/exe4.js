750a493adcd895a9125d60ad1e8fada86d8332a2
mgluz <miguelmluz777@gmail.com>
2024-12-17 16:42:31 -0300

    l2exe4

--- Exercicios/javascript/lib/lista2/exe4.js
+++ Exercicios/javascript/lib/lista2/exe4.js
@@ -1,13 +1,7 @@
 // 4. Escreva uma função que receba dois números e imprime o resultado da multiplicação entre os
 // números.
-function produto (x, y) { 
-    var n1 = (x);
-    var n2 = (y);
-           
-    result = (x * y);
-  
-    return result;
-  }
-  console.log(produto(3, 7));
-  console.log(produto(6, 5));
-  console.log(produto(-2, 8));
+import { produto } from "./bibliotecal2.js";
+
+console.log(produto(3, 7));
+console.log(produto(6, 5));
+console.log(produto(-2, 8));

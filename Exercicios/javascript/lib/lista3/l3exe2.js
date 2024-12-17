933376ce1b345455831e5f24bb76fe5e0701c604
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:02:50 -0300

    l3exe2

--- Exercicios/javascript/lib/lista3/l3exe2.js
+++ Exercicios/javascript/lib/lista3/l3exe2.js
@@ -1,15 +1,10 @@
-// function n(par) {
-//     if (par % 2 === 0) {
-//         return "Par"
-//     } else
-//     return "impar"
-// }
+// 2. Verifique se um número é ímpar ou par
+import { parOuImpar } from "./bibliotecal3.js";
+// const pares = lista.filter((valor) => valor % 2 === 0);
+// const total = lista.reduce(soma, 0);
+// console.log(pares);
 
-const pares = lista.filter((valor) => valor % 2 === 0);
-const total = lista.reduce(soma, 0);
-console.log(pares);
-
-// console.log(n(4))
-// console.log(n(7))
-// console.log(n(0))
-// console.log(n(-5))
+console.log(parOuImpar(4))
+console.log(parOuImpar(7))
+console.log(parOuImpar(0))
+console.log(parOuImpar(-5))

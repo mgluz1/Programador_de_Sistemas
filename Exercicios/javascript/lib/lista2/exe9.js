a9ef2fc7571ee68dd43826ea914c23790b2604e6
mgluz <miguelmluz777@gmail.com>
2024-12-17 16:49:20 -0300

    l2exe9

--- Exercicios/javascript/lib/lista2/exe9.js
+++ Exercicios/javascript/lib/lista2/exe9.js
@@ -1,10 +1,6 @@
 // 9. Retorne o resto da divisão entre dois números
-function resto (x, y) {
-    var n1 = Number(x) 
-    var n2 = Number(y)
-    var result = n1 % n2
-    return result   
-}
+import { resto } from "./bibliotecal2.js";
+
 console.log(resto(20,3));
 console.log(resto(25,5));
 console.log(resto(100,10));

7d13697d7c741087c5360201d60923a154d874e8
mgluz <miguelmluz777@gmail.com>
2024-12-17 16:42:50 -0300

    l2exe6

--- Exercicios/javascript/lib/lista2/exe6.js
+++ Exercicios/javascript/lib/lista2/exe6.js
@@ -1,14 +1,8 @@
 // 6. Escreva uma função que retorne o resultado da divisão inteira entre dois números inteiros.
-function divisao (x, y) { 
-    var n1 = (x);
-    var n2 = (y);
-           
-    result = Math.floor(x / y);
-  
-    return result;
-  }
-  console.log(divisao(10, 3));
-  console.log(divisao(9, 3));
-  console.log(divisao(9, 2));
-  console.log(divisao(7, 5));
-  console.log(divisao(0, 5));
+import { divisao } from "./bibliotecal2.js";
+
+console.log(divisao(10, 3));
+console.log(divisao(9, 3));
+console.log(divisao(9, 2));
+console.log(divisao(7, 5));
+console.log(divisao(0, 5));

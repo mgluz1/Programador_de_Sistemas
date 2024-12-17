cd84e6e67b847c1ab0ccd463513601677d22beeb
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:11:31 -0300

    l3exe13

--- Exercicios/javascript/lib/lista3/l3exe13.js
+++ Exercicios/javascript/lib/lista3/l3exe13.js
@@ -1,18 +1,4 @@
-function tabuadaCompleta() {
+// 13. Desafio Crie um procedimento que imprima a TODA tabuada da multiplicação
+import { tabuadaCompleta } from "./bibliotecal3.js";
 
-    for (var x = 1; x <= 10; x ++ ) {
-        
-       for (var n = 1; n <= 10; n++) {
-            
-     var n2 = n * x 
-     let linha = `${x} x ${n} = ${n2}`
-    
-     console.log(linha)
-    }
-    
-    }    
-  }
-  console.log(tabuadaCompleta())
-         
-   //console.log(n)
-   //console.log(x)
+console.log(tabuadaCompleta())

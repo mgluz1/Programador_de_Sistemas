4aee03db19557e1d9d295dc70602d2d5d58edaec
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:04:09 -0300

    l3exe7

--- Exercicios/javascript/lib/lista3/l3exe7.js
+++ Exercicios/javascript/lib/lista3/l3exe7.js
@@ -1,9 +1,6 @@
-function verificarAnoBissexto(ano) {
-    if(ano % 400 === 0) return true;
-    if(ano % 100 === 0) return false;
-    if(ano %   4 === 0) return true;
-    return false;
-}
+// 7. Desafio Verifique se um ano é bissexto
+import { verificarAnoBissexto } from "./bibliotecal3.js";
+
 console.log(verificarAnoBissexto(2020));  // true
 console.log(verificarAnoBissexto(2024));  // true
 console.log(verificarAnoBissexto(1999));  // false

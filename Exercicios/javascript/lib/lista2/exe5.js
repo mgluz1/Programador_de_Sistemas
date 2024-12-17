0b87e90049b4655cf3d13749f10c8c44e4d5a026
mgluz <miguelmluz777@gmail.com>
2024-12-17 16:42:39 -0300

    l2exe5

--- Exercicios/javascript/lib/lista2/exe5.js
+++ Exercicios/javascript/lib/lista2/exe5.js
@@ -1,13 +1,6 @@
 // 5. Escreva uma função que recebe três números e retorna a média deles.
-function media (x, y, z) { 
-    var n1 = (x);
-    var n2 = (y);
-    var n3 = (z);
-           
-    result = ((x + y + z)/3);
-  
-    return result;
-  }
-  console.log(media(10, 20, 30));
-  console.log(media(5, 15, 25));
-  console.log(media(2, 2, 2));
+import { media } from "./bibliotecal2.js";
+
+console.log(media(10, 20, 30));
+console.log(media(5, 15, 25));
+console.log(media(2, 2, 2));

36e87d6300a745528248f5d50e84864d3a4da4ec
mgluz <miguelmluz777@gmail.com>
2024-12-17 16:41:27 -0300

    l2exe2

--- Exercicios/javascript/lib/lista2/exe2.js
+++ Exercicios/javascript/lib/lista2/exe2.js
@@ -1,4 +1,5 @@
 // 2. Escreva um procedimento que solicita um nome e depois exiba um alerta com a mensagem "Olá,
 // [nome]!".
-var nome = prompt("Qual é o seu nome?");
-  alert("Olá, " + nome + "!");
+import { OlaNome } from "./bibliotecal2.js";
+
+console.log(OlaNome('Miguel'));

fd56f9c681f4fddaa3780eda8301fcb9cce9593e
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:03:20 -0300

    l3exe5

--- Exercicios/javascript/lib/lista3/l3exe5.js
+++ Exercicios/javascript/lib/lista3/l3exe5.js
@@ -1,19 +1,7 @@
-function obeterMaior(a,b,c) {
-    let maior
-    if (a > b) {
-        maior = a
-    } else {
-        maior = b
-    }
-    if (c > maior) {
-        maior = c
-    } else{
-        maior = maior
-    }
-     
-   return maior
-}
+// 5. Encontre o maior de três números utilizando apenas estruturas de controle (i.e. sem utilizar
+// a biblioteca Math)
+import { obterMaior } from "./bibliotecal3.js";
 
-console.log(obeterMaior(3,5,7))
-console.log(obeterMaior(-1,-5,-3))
-console.log(obeterMaior(8,2,10))
+console.log(obterMaior(3,5,7))
+console.log(obterMaior(-1,-5,-3))
+console.log(obterMaior(8,2,10))

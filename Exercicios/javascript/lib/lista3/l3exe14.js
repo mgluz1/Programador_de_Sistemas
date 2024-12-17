7cdab82de000d1d9300a55d90f28088f7b226ff4
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:11:53 -0300

    l3exe14

---
+++ Exercicios/javascript/lib/lista3/l3exe14.js
@@ -0,0 +1,15 @@
+// 14. Desafio Verifique se um número é primo
+import { encontrarPrimo } from "./bibliotecal3.js";
+
+console.log(encontrarPrimo(7))
+console.log(encontrarPrimo(10))
+console.log(encontrarPrimo(13))
+console.log(encontrarPrimo(0))
+console.log(encontrarPrimo(1))
+console.log(encontrarPrimo(2))
+console.log(encontrarPrimo(327))
+console.log(encontrarPrimo(847))
+console.log(encontrarPrimo(1573))
+console.log(encontrarPrimo(31))
+console.log(encontrarPrimo(-3))
+console.log(encontrarPrimo(-31))

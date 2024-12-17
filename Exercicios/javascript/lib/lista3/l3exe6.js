f57a05c0557cdf0429139ae8c3df92d393caf919
mgluz <miguelmluz777@gmail.com>
2024-12-17 17:03:59 -0300

    l3exe6

--- Exercicios/javascript/lib/lista3/l3exe6.js
+++ Exercicios/javascript/lib/lista3/l3exe6.js
@@ -1,10 +1,6 @@
-function multiplos(n1,n2) {
-    if (n1 % n2 === 0) {
-        return "True"
-    } else {
-        return "False"
-    }
-}
+// 6. Verifique se dois números são múltiplos
+import { multiplos } from "./bibliotecal3.js";
+
 console.log(multiplos(10,5))
 console.log(multiplos(12,7))
 console.log(multiplos(20,4))
